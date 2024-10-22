1014d80822f5db99dfbe366c57e15821869289ce
Isaac <isaaclatta73@gmail.com>
2024-10-22 10:27:39 -0700

    Added initial files

---
+++ build/CMakeFiles/myapp.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/isaac/URepair/third_party/glad/glad.c" "CMakeFiles/myapp.dir/third_party/glad/glad.c.o" "gcc" "CMakeFiles/myapp.dir/third_party/glad/glad.c.o.d"
+  "/home/isaac/URepair/src/main.cpp" "CMakeFiles/myapp.dir/src/main.cpp.o" "gcc" "CMakeFiles/myapp.dir/src/main.cpp.o.d"
+  "/home/isaac/URepair/third_party/imgui/backends/imgui_impl_glfw.cpp" "CMakeFiles/myapp.dir/third_party/imgui/backends/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/myapp.dir/third_party/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "/home/isaac/URepair/third_party/imgui/backends/imgui_impl_opengl3.cpp" "CMakeFiles/myapp.dir/third_party/imgui/backends/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/myapp.dir/third_party/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "/home/isaac/URepair/third_party/imgui/imgui.cpp" "CMakeFiles/myapp.dir/third_party/imgui/imgui.cpp.o" "gcc" "CMakeFiles/myapp.dir/third_party/imgui/imgui.cpp.o.d"
+  "/home/isaac/URepair/third_party/imgui/imgui_draw.cpp" "CMakeFiles/myapp.dir/third_party/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/myapp.dir/third_party/imgui/imgui_draw.cpp.o.d"
+  "/home/isaac/URepair/third_party/imgui/imgui_tables.cpp" "CMakeFiles/myapp.dir/third_party/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/myapp.dir/third_party/imgui/imgui_tables.cpp.o.d"
+  "/home/isaac/URepair/third_party/imgui/imgui_widgets.cpp" "CMakeFiles/myapp.dir/third_party/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/myapp.dir/third_party/imgui/imgui_widgets.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/myapp.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myapp.dir/src/main.cpp.o"
+  "CMakeFiles/myapp.dir/src/main.cpp.o.d"
+  "CMakeFiles/myapp.dir/third_party/glad/glad.c.o"
+  "CMakeFiles/myapp.dir/third_party/glad/glad.c.o.d"
+  "CMakeFiles/myapp.dir/third_party/imgui/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/myapp.dir/third_party/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/myapp.dir/third_party/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/myapp.dir/third_party/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/myapp.dir/third_party/imgui/imgui.cpp.o"
+  "CMakeFiles/myapp.dir/third_party/imgui/imgui.cpp.o.d"
+  "CMakeFiles/myapp.dir/third_party/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/myapp.dir/third_party/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/myapp.dir/third_party/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/myapp.dir/third_party/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/myapp.dir/third_party/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/myapp.dir/third_party/imgui/imgui_widgets.cpp.o.d"
+  "myapp"
+  "myapp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/myapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
