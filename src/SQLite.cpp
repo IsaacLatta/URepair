4a7cfd96637c5fc1056543111984e2f676b06565
Isaac <isaaclatta73@gmail.com>
2024-11-20 18:02:54 -0800

    Added sqlite

--- src/SQLite.cpp
+++ src/SQLite.cpp
@@ -4,16 +4,18 @@ bool SQLite::connect() {
     return true;
 }
 
-bool run_query(const char* query, void* data)
+bool run_query(const char* query, void* data) {
+    return true;
+}
 
 static int loaddata_callback(void* user_obj, int argc, char**argv, char** col_name) {
     User* user = static_cast<User*>(user_obj);
 
 
-
+    return 0;
 
 }
 
 bool SQLite::loadData(User* user) {
-
+    return true;
 }
