ef2eb932124ddcef20bc144e90a8282063564c84
Isaac <isaaclatta73@gmail.com>
2024-11-20 18:00:22 -0800

    Began adding sqlite class

---
+++ src/SQLite.cpp
@@ -0,0 +1,19 @@
+#include "Database.h"
+
+bool SQLite::connect() {
+    return true;
+}
+
+bool run_query(const char* query, void* data)
+
+static int loaddata_callback(void* user_obj, int argc, char**argv, char** col_name) {
+    User* user = static_cast<User*>(user_obj);
+
+
+
+
+}
+
+bool SQLite::loadData(User* user) {
+
+}
